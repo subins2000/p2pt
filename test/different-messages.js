60fd06b53e3fd0312c62624c0a864b54a460ebd5
Subin Siby <mail@subinsb.com>
2020-08-01 01:43:10 +0530

    Add new test

---
+++ test/different-messages.js
@@ -0,0 +1,44 @@
+const test = require('tape')
+
+if (process.env['BROWSER_TEST']) {
+  var P2PT = require('../p2pt')
+} else {
+  var P2PT = require('../node')
+}
+
+const announceURLs = [
+  'ws://localhost:5000'
+  // 'wss://tracker.btorrent.xyz:443/announce'
+]
+
+const randomString = (length) => {
+  return [...Array(length)]
+    .map(() => (~~(Math.random() * 36)).toString(36))
+    .join('');
+};
+
+test('large message', function (t) {
+  var p2pt1 = new P2PT(announceURLs, 'p2pt')
+  var p2pt2 = new P2PT(announceURLs, 'p2pt')
+
+  const KB100 = randomString(100000) // 100KB
+  const KB1000 = randomString(1000000) // 1MB
+
+  p2pt1.on('peerconnect', (peer) => {
+    p2pt1.send(peer, KB100).then(([peer, msg]) => {
+      t.equal(msg, KB1000)
+
+      p2pt1.destroy()
+      p2pt2.destroy()
+      t.end()
+    })
+  })
+
+  p2pt2.on('msg', (peer, msg) => {
+    t.equal(msg, KB100)
+    peer.respond(KB1000)
+  })
+
+  p2pt1.start()
+  p2pt2.start()
+})
