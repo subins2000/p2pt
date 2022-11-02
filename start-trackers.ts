4fc4f4a8f1fddff9ead878f5a165c4fb5e28cd26
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2022-11-02 14:53:57 -0700

    begin revising tests

---
+++ start-trackers.ts
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process';
+
+const log = (data: unknown) => {
+  console.log(`stdout: ${data}`);
+};
+
+const server1 = spawn('node', ['startTracker.js']);
+const server2 = spawn('env', ['PORT=5001', 'node', 'startTracker.js']);
+
+server1.stdout.on('data', log);
+server1.stderr.on('data', log);
+server1.on('error', log);
+server1.on('close', log);
+
+server2.stdout.on('data', log);
+server2.stderr.on('data', log);
+server2.on('error', log);
+server2.on('close', log);
