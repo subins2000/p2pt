4dbaab7566841479e94faee9af83fd1d41d7f90e
Daniel Meyer <8926560+pubkey@users.noreply.github.com>
2022-11-17 15:11:50 +0100

    FIX broken typings

--- p2pt.d.ts
+++ p2pt.d.ts
@@ -2,14 +2,14 @@ declare module "p2pt" {
   import type { EventEmitter } from "events";
   export default class P2PT<SendableMessage = any> extends EventEmitter {
     _peerId: string;
-    constructor(announceURLs: Array<string> = [], identifierString = "");
+    constructor(announceURLs?: Array<string>, identifierString?: string);
     setIdentifier(identifierString: string): void;
     start(): void;
     requestMorePeers(): Promise<object>;
     send(
       peer: Peer,
       msg: SendableMessage,
-      msgID: number = ""
+      msgID?: number
     ): Promise<[peer: Peer, msg: object]>;
     addTracker(announceURL: string): void;
     removeTracker(announceURL: string): void;
@@ -17,20 +17,20 @@ declare module "p2pt" {
     getTrackerStats(): TrackerStats;
     destroy(): void;
 
-    on(event: "peerconnect", callback: (peer: Peer) => void);
-    on(event: "data", callback: (peer: Peer, data: any) => void);
-    on(event: "msg", callback: (peer: Peer, msg: any) => void);
-    on(event: "peerclose", callback: (peer: Peer) => void);
+    on(event: "peerconnect", callback: (peer: Peer) => void): void;
+    on(event: "data", callback: (peer: Peer, data: any) => void): void;
+    on(event: "msg", callback: (peer: Peer, msg: any) => void): void;
+    on(event: "peerclose", callback: (peer: Peer) => void): void;
     on(
       event: "trackerconnect",
       callback: (tracker: Tracker, stats: TrackerStats) => void
-    );
+    ): void;
     on(
       event: "trackerwarning",
       callback: (error: object, stats: TrackerStats) => void
-    );
+    ): any;
   }
-  export interface Peer {
+  export interface Peer<SendableMessage = any> {
     id: string;
     respond(msg: SendableMessage): Promise<[peer: Peer, msg: any]>;
   }
